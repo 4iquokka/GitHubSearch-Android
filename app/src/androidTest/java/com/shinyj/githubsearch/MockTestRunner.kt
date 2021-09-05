dac7bbca3f13dd4db3e502ed5f1d38bcb5bd1601
YJ SHiN <enzeroment@SHiNYJ-MBP13.local>
2021-09-06 00:47:12 +0900

    resolve #9 : Setup the instrumentation test environment (Hilt)

---
+++ app/src/androidTest/java/com/shinyj/githubsearch/MockTestRunner.kt
@@ -0,0 +1,17 @@
+package com.shinyj.githubsearch
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+class MockTestRunner : AndroidJUnitRunner() {
+
+    override fun newApplication(
+        cl: ClassLoader?,
+        className: String?,
+        context: Context?
+    ): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+}
---
+++ app/src/debug/java/com/shinyj/githubsearch/HiltTestActivity.kt
@@ -0,0 +1,9 @@
+package com.shinyj.githubsearch
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity() {
+
+}
