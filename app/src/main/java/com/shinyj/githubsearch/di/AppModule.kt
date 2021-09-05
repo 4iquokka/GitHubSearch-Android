19609cbe6033304932f4e6bcd43b3b8d198b363f
YJ SHiN <enzeroment@SHiNYJ-MBP13.local>
2021-09-05 23:19:24 +0900

    close #7 : Setup the dependency injection with Hilt

--- app/src/main/java/com/shinyj/githubsearch/di/AppModule.kt
+++ app/src/main/java/com/shinyj/githubsearch/di/AppModule.kt
@@ -1,4 +1,12 @@
 package com.shinyj.githubsearch.di
 
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+
 }
--- app/src/main/java/com/shinyj/githubsearch/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/shinyj/githubsearch/presentation/search/SearchFragment.kt
@@ -1,7 +1,9 @@
 package com.shinyj.githubsearch.presentation.search
 
 import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SearchFragment : Fragment() {
 
 }
