58a4087a9932eab7db4dd9e7440a86e7d2bf3aae
shinyj9238 <m.shinyj9238@gmail.com>
2021-09-08 09:17:05 +0900

    Resolve #13 : Implement DateUtils, StringUtils, and common-use Utils

--- app/src/main/java/com/shinyj/githubsearch/util/DateUtils.kt
+++ app/src/main/java/com/shinyj/githubsearch/util/DateUtils.kt
@@ -1,7 +1,25 @@
 package com.shinyj.githubsearch.util
 
+import android.os.Build
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
+
 object DateUtils {
 
-
+    fun formatDateString(dateString : String) : String{
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH)
+            val outputFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd", Locale.ENGLISH)
+            val date = LocalDate.parse(dateString, inputFormatter)
+            outputFormatter.format(date)
+        } else {
+            val inputFormatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH)
+            val outputFormatter = SimpleDateFormat("yyyy.MM.dd", Locale.ENGLISH)
+            val date = inputFormatter.parse(dateString)
+            outputFormatter.format(date!!)
+        }
+    }
 
 }
